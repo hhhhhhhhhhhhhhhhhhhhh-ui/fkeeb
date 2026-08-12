06224274f4cb723e2daccfbd63e4df1e5c86f6cf
hhhhhhhhhhhhhhhhhhhhh-ui <sandeepsingh738966@gmail.com>
2026-08-12 15:38:29 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,325 @@
+const productsData = {
+    "3001": {
+    "id": 3001,
+    "filename": "product3001.html",
+    "title": "Sasimo 1-Wardrobe 4-Door 4-Shelf Plastic Foldable Cupboard PC Collapsible Wardrobe - Flipkart",
+    "description": "Buy Sasimo 1-Wardrobe 4-Door 4-Shelf Plastic Foldable Cupboard PC Collapsible Wardrobe at Flipkart. 78% Off!",
+    "name": "Sasimo 1-Wardrobe 4-Door 4-Shelf Plastic Foldable Cupboard PC Collapsible Wardrobe",
+    "rating_val": "4.0 ★",
+    "rating_count": "2,306 Ratings",
+    "discount": "78% Off",
+    "original_price": "₹899",
+    "sale_price": "₹199",
+    "wow_price": "₹199",
+    "images": [
+        "https://rukminim2.flixcart.com/image/612/612/xif0q/collapsible-wardrobe/a/w/w/375-pc-690-0-3-metal-and-plastic-shoe-stand-collapsible-wardrobe-original-imahg4th9zbwagk5.jpeg?q=70"
+    ],
+    "colors": []
+},
+    "3002": {
+    "id": 3002,
+    "filename": "product3002.html",
+    "title": "ELINDOR 5-Door 5-Shelf Plastic Collapsible Cupboard PC Wardrobe (White-5) - Flipkart",
+    "description": "Buy ELINDOR 5-Door 5-Shelf Plastic Collapsible Cupboard PC Wardrobe (White-5) at Flipkart. 75% Off!",
+    "name": "ELINDOR 5-Door 5-Shelf Plastic Collapsible Cupboard PC Wardrobe (White-5)",
+    "rating_val": "5.0 ★",
+    "rating_count": "82 Ratings",
+    "discount": "75% Off",
+    "original_price": "₹999",
+    "sale_price": "₹249",
+    "wow_price": "₹249",
+    "images": [
+        "https://rukminim2.flixcart.com/image/612/612/xif0q/collapsible-wardrobe/i/l/8/370-pc-750-0-3-plastic-pp-collapsible-wardrobe-8-door-12-shelves-original-imahfg5dnmu9bmre.jpeg?q=70"
+    ],
+    "colors": []
+},
+    "3003": {
+    "id": 3003,
+    "filename": "product3003.html",
+    "title": "ELINDOR 4-Door 4-Shelf Plastic Collapsible Cupboard PC Wardrobe (White) - Flipkart",
+    "description": "Buy ELINDOR 4-Door 4-Shelf Plastic Collapsible Cupboard PC Wardrobe (White) at Flipkart. 73% Off!",
+    "name": "ELINDOR 4-Door 4-Shelf Plastic Collapsible Cupboard PC Wardrobe (White)",
+    "rating_val": "4.5 ★",
+    "rating_count": "64 Ratings",
+    "discount": "73% Off",
+    "original_price": "₹899",
+    "sale_price": "₹239",
+    "wow_price": "₹239",
+    "images": [
+        "https://images.price.tools/images/mevigo-4-door-4-shelf-plastic-m-TP8Wioqs.jpg"
+    ],
+    "colors": []
+},
+    "3004": {
+    "id": 3004,
+    "filename": "product3004.html",
+    "title": "Mevigo 9-Door Plastic Wardrobe for Kids | Almirah Organizer Cupboard Rack (White-TG651) - Flipkart",
+    "description": "Buy Mevigo 9-Door Plastic Wardrobe for Kids | Almirah Organizer Cupboard Rack (White-TG651) at Flipkart. 75% Off!",
+    "name": "Mevigo 9-Door Plastic Wardrobe for Kids | Almirah Organizer Cupboard Rack (White-TG651)",
+    "rating_val": "4.0 ★",
+    "rating_count": "586 Ratings",
+    "discount": "75% Off",
+    "original_price": "₹1999",
+    "sale_price": "₹499",
+    "wow_price": "₹499",
+    "images": [
+        "https://m.media-amazon.com/images/I/412w1HI4rZL.jpg"
+    ],
+    "colors": []
+},
+    "3005": {
+    "id": 3005,
+    "filename": "product3005.html",
+    "title": "Rosebell 1-Person Foldable Sofa Cum Bed | Grey Jute Fabric | 3x6 Ft | Washable Cover & Cushion - Flipkart",
+    "description": "Buy Rosebell 1-Person Foldable Sofa Cum Bed | Grey Jute Fabric | 3x6 Ft | Washable Cover & Cushion at Flipkart. 73% Off!",
+    "name": "Rosebell 1-Person Foldable Sofa Cum Bed | Grey Jute Fabric | 3x6 Ft | Washable Cover & Cushion",
+    "rating_val": "4.0 ★",
+    "rating_count": "40 Ratings",
+    "discount": "73% Off",
+    "original_price": "₹1999",
+    "sale_price": "₹549",
+    "wow_price": "₹549",
+    "images": [
+        "https://m.media-amazon.com/images/I/41-TxT9cbUL.jpg"
+    ],
+    "colors": []
+},
+    "3006": {
+    "id": 3006,
+    "filename": "product3006.html",
+    "title": "BonKaso Steel Foldable Cloth Dryer Stand | 2-Level Stainless Steel Drying Rack | Portable - Flipkart",
+    "description": "Buy BonKaso Steel Foldable Cloth Dryer Stand | 2-Level Stainless Steel Drying Rack | Portable at Flipkart. 80% Off!",
+    "name": "BonKaso Steel Foldable Cloth Dryer Stand | 2-Level Stainless Steel Drying Rack | Portable",
+    "rating_val": "4.2 ★",
+    "rating_count": "1,847 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹999",
+    "sale_price": "₹199",
+    "wow_price": "₹199",
+    "images": [
+        "https://m.media-amazon.com/images/I/61gQFaujEfL._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3007": {
+    "id": 3007,
+    "filename": "product3007.html",
+    "title": "Ozoy Heavy-Duty Garment Stand | Cloth Rack with Bottom Shelves | Adjustable Coat Hanger (RAIN-BLACK) - Flipkart",
+    "description": "Buy Ozoy Heavy-Duty Garment Stand | Cloth Rack with Bottom Shelves | Adjustable Coat Hanger (RAIN-BLACK) at Flipkart. 80% Off!",
+    "name": "Ozoy Heavy-Duty Garment Stand | Cloth Rack with Bottom Shelves | Adjustable Coat Hanger (RAIN-BLACK)",
+    "rating_val": "4.1 ★",
+    "rating_count": "734 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹999",
+    "sale_price": "₹199",
+    "wow_price": "₹199",
+    "images": [
+        "https://m.media-amazon.com/images/I/71LacGjtMIL._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3008": {
+    "id": 3008,
+    "filename": "product3008.html",
+    "title": "Loom & Needles Rollaway Bed with Mattress | Metal Single Foldable Bed | Lockable Wheels | 6-Inch Foam - Flipkart",
+    "description": "Buy Loom & Needles Rollaway Bed with Mattress | Metal Single Foldable Bed | Lockable Wheels | 6-Inch Foam at Flipkart. 80% Off!",
+    "name": "Loom & Needles Rollaway Bed with Mattress | Metal Single Foldable Bed | Lockable Wheels | 6-Inch Foam",
+    "rating_val": "3.9 ★",
+    "rating_count": "60 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹2999",
+    "sale_price": "₹599",
+    "wow_price": "₹599",
+    "images": [
+        "https://m.media-amazon.com/images/I/61-AppEvW4L._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3009": {
+    "id": 3009,
+    "filename": "product3009.html",
+    "title": "KriShyam 16-Cube Storage Cabinet with Doors | 2 Hanging Rods | PP Collapsible Modular Wardrobe | White - Flipkart",
+    "description": "Buy KriShyam 16-Cube Storage Cabinet with Doors | 2 Hanging Rods | PP Collapsible Modular Wardrobe | White at Flipkart. 80% Off!",
+    "name": "KriShyam 16-Cube Storage Cabinet with Doors | 2 Hanging Rods | PP Collapsible Modular Wardrobe | White",
+    "rating_val": "3.5 ★",
+    "rating_count": "20 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹1999",
+    "sale_price": "₹399",
+    "wow_price": "₹399",
+    "images": [
+        "https://m.media-amazon.com/images/I/613mU+fyn-L._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3010": {
+    "id": 3010,
+    "filename": "product3010.html",
+    "title": "Nilkamal Joyce Engineered Wood 4 Door Wardrobe | Classic Walnut Finish | Almirah with Shelves & Hanging - Flipkart",
+    "description": "Buy Nilkamal Joyce Engineered Wood 4 Door Wardrobe | Classic Walnut Finish | Almirah with Shelves & Hanging at Flipkart. 80% Off!",
+    "name": "Nilkamal Joyce Engineered Wood 4 Door Wardrobe | Classic Walnut Finish | Almirah with Shelves & Hanging",
+    "rating_val": "3.9 ★",
+    "rating_count": "1,276 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹3499",
+    "sale_price": "₹699",
+    "wow_price": "₹699",
+    "images": [
+        "https://m.media-amazon.com/images/I/61+BdX3TpgL._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3011": {
+    "id": 3011,
+    "filename": "product3011.html",
+    "title": "FPH Julian Engineered Wood 2 Door Wardrobe | American Espresso | Mirror | European Hinges | Bestseller - Flipkart",
+    "description": "Buy FPH Julian Engineered Wood 2 Door Wardrobe | American Espresso | Mirror | European Hinges | Bestseller at Flipkart. 80% Off!",
+    "name": "FPH Julian Engineered Wood 2 Door Wardrobe | American Espresso | Mirror | European Hinges | Bestseller",
+    "rating_val": "3.6 ★",
+    "rating_count": "55,134 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹1499",
+    "sale_price": "₹299",
+    "wow_price": "₹299",
+    "images": [
+        "https://m.media-amazon.com/images/I/61zDPQktLYL._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "3012": {
+    "id": 3012,
+    "filename": "product3012.html",
+    "title": "FPH Julian Engineered Wood 2 Door Wardrobe | Melamine Wenge | Mirror | European Hinges | Premium Finish - Flipkart",
+    "description": "Buy FPH Julian Engineered Wood 2 Door Wardrobe | Melamine Wenge | Mirror | European Hinges | Premium Finish at Flipkart. 80% Off!",
+    "name": "FPH Julian Engineered Wood 2 Door Wardrobe | Melamine Wenge | Mirror | European Hinges | Premium Finish",
+    "rating_val": "3.6 ★",
+    "rating_count": "13,508 Ratings",
+    "discount": "80% Off",
+    "original_price": "₹1449",
+    "sale_price": "₹289",
+    "wow_price": "₹289",
+    "images": [
+        "https://m.media-amazon.com/images/I/71zCmur8yUL._SX679_.jpg"
+    ],
+    "colors": []
+},
+    "8001": {
+    "id": 8001,
+    "filename": "product8001.html",
+    "title": "MEVIGO 4-Door 2-Shelf Plastic Kids Collapsible Cupboard | PC Wardrobe | White | DIY Tool-Free - Flipkart",
+    "description": "Buy MEVIGO 4-Door 2-Shelf Plastic Kids Collapsible Cupboard | PC Wardrobe | White | DIY Tool-Free at Flipkart. 97% Off!",
+    "name": "MEVIGO 4-Door 2-Shelf Plastic Kids Collapsible Cupboard | PC Wardrobe | White | DIY Tool-Free",
+    "rating_val": "4.1 ★",
+    "rating_count": "11,983 Ratings",
+    "discount": "97% Off",
+    "original_price": "₹4661",
+    "sale_price": "₹149",
+    "wow_price": "₹149",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8001-1.jpg"
+    ],
+    "colors": []
+},
+    "8003": {
+    "id": 8003,
+    "filename": "product8003.html",
+    "title": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Tool-Free Assembly - Flipkart",
+    "description": "Buy Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Tool-Free Assembly at Flipkart. 97% Off!",
+    "name": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Tool-Free Assembly",
+    "rating_val": "3.8 ★",
+    "rating_count": "7,841 Ratings",
+    "discount": "97% Off",
+    "original_price": "₹6211",
+    "sale_price": "₹199",
+    "wow_price": "₹199",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8003-1.jpg"
+    ],
+    "colors": []
+},
+    "8004": {
+    "id": 8004,
+    "filename": "product8004.html",
+    "title": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | Large PC Collapsible Wardrobe | Extra-Wide Compartments - Flipkart",
+    "description": "Buy Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | Large PC Collapsible Wardrobe | Extra-Wide Compartments at Flipkart. 97% Off!",
+    "name": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | Large PC Collapsible Wardrobe | Extra-Wide Compartments",
+    "rating_val": "4.4 ★",
+    "rating_count": "11,983 Ratings",
+    "discount": "97% Off",
+    "original_price": "₹8784",
+    "sale_price": "₹249",
+    "wow_price": "₹249",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8004-1.jpg"
+    ],
+    "colors": []
+},
+    "8005": {
+    "id": 8005,
+    "filename": "product8005.html",
+    "title": "Sasimo 6-Door 6-Shelf Plastic Foldable Cupboard | XL PC Collapsible Wardrobe | Maximum Storage - Flipkart",
+    "description": "Buy Sasimo 6-Door 6-Shelf Plastic Foldable Cupboard | XL PC Collapsible Wardrobe | Maximum Storage at Flipkart. 98% Off!",
+    "name": "Sasimo 6-Door 6-Shelf Plastic Foldable Cupboard | XL PC Collapsible Wardrobe | Maximum Storage",
+    "rating_val": "4.3 ★",
+    "rating_count": "2,871 Ratings",
+    "discount": "98% Off",
+    "original_price": "₹10397",
+    "sale_price": "₹249",
+    "wow_price": "₹249",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8005-1.jpg"
+    ],
+    "colors": []
+},
+    "8006": {
+    "id": 8006,
+    "filename": "product8006.html",
+    "title": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dual-Tone Design - Flipkart",
+    "description": "Buy Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dual-Tone Design at Flipkart. 97% Off!",
+    "name": "Sasimo 5-Door 5-Shelf Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dual-Tone Design",
+    "rating_val": "3.9 ★",
+    "rating_count": "2,871 Ratings",
+    "discount": "97% Off",
+    "original_price": "₹6085",
+    "sale_price": "₹199",
+    "wow_price": "₹199",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8006-1.jpg"
+    ],
+    "colors": []
+},
+    "8007": {
+    "id": 8007,
+    "filename": "product8007.html",
+    "title": "Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Space-Saving - Flipkart",
+    "description": "Buy Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Space-Saving at Flipkart. 96% Off!",
+    "name": "Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Space-Saving",
+    "rating_val": "4.3 ★",
+    "rating_count": "6,217 Ratings",
+    "discount": "96% Off",
+    "original_price": "₹5356",
+    "sale_price": "₹189",
+    "wow_price": "₹189",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8007-1.jpg"
+    ],
+    "colors": []
+},
+    "8008": {
+    "id": 8008,
+    "filename": "product8008.html",
+    "title": "Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dark Finish - Flipkart",
+    "description": "Buy Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dark Finish at Flipkart. 97% Off!",
+    "name": "Sasimo 4-Door 4-Shelf Compact Plastic Foldable Cupboard | PC Collapsible Wardrobe | Dark Finish",
+    "rating_val": "4.2 ★",
+    "rating_count": "4,089 Ratings",
+    "discount": "97% Off",
+    "original_price": "₹6714",
+    "sale_price": "₹189",
+    "wow_price": "₹189",
+    "images": [
+        "https://jo128.surigq.shop/cosmicAssets/furnitureImages/product-8008-1.jpg"
+    ],
+    "colors": []
+}
+};
